a803c34fbe5ce40c87bf7ef7146941b7721776aa
Sashminda Withanage <sashmindaps4@gmail.com>
2021-08-31 10:08:29 +0530

    Base project structure created

---
+++ sticky-notes/src/Routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NoteArea } from "./components/NoteArea";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export const Routes = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path={"/notearea"} component={NoteArea} />
+          <Redirect exact from="/" to="/notearea" />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
---
+++ sticky-notes/src/components/NoteArea.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NoteArea = () => {
+  return <div>YO</div>;
+};
